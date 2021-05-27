9e6c703c8e5df107405a91773bf9819036daf273
davi-pou07 <davihareis@gmail.com>
2021-05-27 18:58:33 -0300

    ok

--- controller/produtoControler.js
+++ controller/produtoControler.js
@@ -2,15 +2,24 @@ const express = require('express')
 const router = express.Router()
 const knex = require("../Databases/admin/databases")
 
-router.get("/produto/:id",(req,res)=>{
+router.get("/produto/:id", (req, res) => {
     produtoId = req.params.id
-    knex("produtos").select().where({id:produtoId}).then(produto =>{
-        knex("categorias").select().where({id:produto[0].categoriaId}).then(categoria =>{
-            knex("imagens").select().where({produtoId:produto[0].id}).then(imagens =>{
-                res.render("produto/pedido",{produto:produto[0],categoria:categoria[0],imagens:imagens})
+    knex("produtos").select().where({ id: produtoId }).then(produto => {
+        knex("produtos").select().then(produtos => {
+            knex("categorias").select().where({ id: produto[0].categoriaId }).then(categoria => {
+                knex("imagens").select().where({ produtoId: produto[0].id }).then(imagems => {
+                    knex("imagens").select().then(imagens => {
+                        knex("precos").select().where({ produtoId: produto[0].id }).then(preco => {
+                            knex("precos").select().then(precos => {
+                                res.render("produto/pedido", { produto: produto[0], categoria: categoria[0], imagems: imagems, preco: preco[0], produtos: produtos, precos: precos, imagens:imagens })
+                            })
+                        })
+                    })
+                })
             })
         })
     })
 })
+
 
 module.exports = router
