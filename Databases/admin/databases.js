029c58ae49f472af3e8aef859e870e3fa7a9e7c4
davi-pou07 <davihareis@gmail.com>
2021-06-28 18:01:51 -0300

    dog

--- Databases/admin/databases.js
+++ Databases/admin/databases.js
@@ -9,6 +9,7 @@ const knex = require('knex')({
       database : 'd9f1ts6ismirrp'
     }
   });
+//postgres://xzohavpuwebfje:cce7fe39b756ba090b9a03883dca985ce43bf137b608cb4cfbc1fcd6ee921e15@ec2-34-200-94-86.compute-1.amazonaws.com:5432/d9f1ts6ismirrp
 
 //postgres://xzohavpuwebfje:cce7fe39b756ba090b9a03883dca985ce43bf137b608cb4cfbc1fcd6ee921e15@ec2-34-200-94-86.compute-1.amazonaws.com:5432/d9f1ts6ismirrp
   module.exports = knex
