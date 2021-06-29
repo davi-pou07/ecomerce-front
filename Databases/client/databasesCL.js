302e3509a27f4b5b01e8776e6f0bb36b9535352d
davi-pou07 <davihareis@gmail.com>
2021-06-28 23:14:53 -0300

    2

--- Databases/client/databasesCL.js
+++ Databases/client/databasesCL.js
@@ -1,15 +1,15 @@
 const Sequelize = require("sequelize");
 
-// const connection = new Sequelize('postgres://postgres:davi6259@localhost:5432/ecomerce')
-const connectionCL = new Sequelize('naeoolvlzfexcm','d6orq1epqv96kp','0097bce388efc76f8d1603d8cd930982f031093997366dc856a3a6aff4b625cc',{
-    host:'ec2-35-169-188-58.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    //configurando timezone
-    timezone: "-03:00"
-})
+const connectionCL = new Sequelize('postgres://naeoolvlzfexcm:0097bce388efc76f8d1603d8cd930982f031093997366dc856a3a6aff4b625cc@ec2-35-169-188-58.compute-1.amazonaws.com:5432/d6orq1epqv96kp')
+// const connectionCL = new Sequelize('naeoolvlzfexcm','d6orq1epqv96kp','0097bce388efc76f8d1603d8cd930982f031093997366dc856a3a6aff4b625cc',{
+//     host:'ec2-35-169-188-58.compute-1.amazonaws.com',
+//     dialect: 'postgres',
+//     //configurando timezone
+//     timezone: "-03:00"
+// })
 module.exports = connectionCL;
-  //postgres://naeoolvlzfexcm:0097bce388efc76f8d1603d8cd930982f031093997366dc856a3a6aff4b625cc@ec2-35-169-188-58.compute-1.amazonaws.com:5432/d6orq1epqv96kp
-
+//postgres://naeoolvlzfexcm:0097bce388efc76f8d1603d8cd930982f031093997366dc856a3a6aff4b625cc@ec2-35-169-188-58.compute-1.amazonaws.com:5432/d6orq1epqv96kp
+//postgres://naeoolvlzfexcm:0097bce388efc76f8d1603d8cd930982f031093997366dc856a3a6aff4b625cc@ec2-35-169-188-58.compute-1.amazonaws.com:5432/d6orq1epqv96kp
 //https://help.heroku.com/DR0TTWWD/seeing-fatal-no-pg_hba-conf-entry-errors-in-postgres
 //https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
 //https://devcenter.heroku.com/articles/heroku-postgresql#heroku-postgres-ssl
