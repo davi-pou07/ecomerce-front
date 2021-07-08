6cfdb3d41f381454d54b356ab9a23eeb796e5d7d
davi-pou07 <davihareis@gmail.com>
2021-07-07 23:22:17 -0300

    td

--- index.js
+++ index.js
@@ -27,16 +27,7 @@ MercadoPago.configure({
     access_token: 'TEST-1254504299447071-061611-ac2150294a43f6a4d65d10f6f66512f8-257758072'
 })
 
-var remetente = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    service: "Outlook365",
-    port: 587,
-    secure: true,
-    auth: {
-        user: "poudeyvis007@gmail.com",
-        pass: "davi6259"
-    }
-});
+
 
 
 const { Pool } = require('pg');
