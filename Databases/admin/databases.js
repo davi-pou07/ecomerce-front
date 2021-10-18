0c80d2e12a2989d073d3e16c2bc84a015919e5dd
davi-pou07 <davihareis@gmail.com>
2021-10-18 08:55:40 -0300

    asda

--- Databases/admin/databases.js
+++ Databases/admin/databases.js
@@ -15,3 +15,7 @@ const knex = require('knex')({
 
 //postgres://xzohavpuwebfje:cce7fe39b756ba090b9a03883dca985ce43bf137b608cb4cfbc1fcd6ee921e15@ec2-34-200-94-86.compute-1.amazonaws.com:5432/d9f1ts6ismirrp
   module.exports = knex
+
+// heroku config:add TZ=America/Recife
+// heroku run bash
+// date
