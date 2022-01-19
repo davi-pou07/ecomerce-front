7578da73d9874c7b64abd7b7a1ff8f8af0a4c590
davi-pou07 <davihareis@gmail.com>
2022-01-19 16:44:08 +0000

    ok

--- controller/atualizaController.js
+++ controller/atualizaController.js
@@ -3,7 +3,7 @@ const router = express()
 const { Sequelize, DataTypes } = require('sequelize')
 const sequelize = require("../Databases/client/databasesCL")
 const queryInterface = sequelize.getQueryInterface();
-
+const CodItens =  require("../Databases/client/CodItens")
 
 // router.get("/atualizarCoditem",(req,res)=>{
 //     queryInterface.addColumn(
@@ -28,5 +28,17 @@ const queryInterface = sequelize.getQueryInterface();
 //       })
 //     res.send("FEITO")
 // })
+router.get("/atualizarCoditem",(req,res)=>{
+    CodItens.findAll().then(coditens =>{
+        coditens.forEach(cod => {
+            if (cod.acredimo == undefined || cod.acredimo == '') {
+                cod.acredimo = 0
+            }
+            if (cod.desconto == undefined || cod.desconto == '') {
+                cod.desconto = 0
+            }
+        });
+    })
+})
 
 module.exports = router
